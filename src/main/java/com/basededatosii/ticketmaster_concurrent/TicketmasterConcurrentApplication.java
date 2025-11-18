b0cf781be6a987f22b1b18c50c9aadecd29babbe
iben polanco <iben.fpolanco@gmail.com>
2025-11-17 22:20:00 -0500

    inicializacion de proyecto

---
+++ src/main/java/com/basededatosii/ticketmaster_concurrent/TicketmasterConcurrentApplication.java
@@ -0,0 +1,13 @@
+package com.basededatosii.ticketmaster_concurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketmasterConcurrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketmasterConcurrentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basededatosii/ticketmaster_concurrent/TicketmasterConcurrentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basededatosii.ticketmaster_concurrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketmasterConcurrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
